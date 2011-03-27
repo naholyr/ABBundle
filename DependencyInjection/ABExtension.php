e3ca88cb6f7c3d677e1c3f91777be8811f8a83d9
Nicolas Chambrier <naholyr@gmail.com>
2011-03-27 23:57:24 +0200

    Refactored configuration layer to allow better overridability

--- DependencyInjection/ABExtension.php
+++ DependencyInjection/ABExtension.php
@@ -19,12 +19,32 @@ class ABExtension extends Extension
 
         $config = $processor->process($configuration->getConfigTree(), $configs);
 
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        try {
-            $loader->load(sprintf('ab-db_driver-%s.yml', $config['db_driver']));
-        } catch (\InvalidArgumentException $e) {
+        // Validate persistence driver
+        $db_driver = $config['db_driver'];
+        if (!in_array($db_driver, array('odm', 'orm', 'custom'))) {
             throw new \InvalidArgumentException('Invalid DB driver for ABBundle, check "ab.db_driver" configuration.');
         }
+
+        // Config = YAML files (yep, I hate XML and it won't change now :P)
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        // Load db_driver's configuration
+        if ($db_driver != 'custom') {
+            $loader->load(sprintf('ab-db_driver-%s.yml', $config['db_driver']));
+        }
+
+        // Eventually override some parameters from driver's config
+        if (!empty($config['model_repository'])) {
+            $container->setParameter('ab.model_repository', $config['model_repository']);
+        }
+        if (!empty($config['model_class'])) {
+            $container->setParameter('ab.model_class', $config['model_class']);
+        }
+
+        // Alias the persistence service (declared as parameter by driver)
+        $container->setAlias('ab.persistence_service', $container->getParameter('ab.persistence_service'));
+
+        // Load service definition, based on previous parameters
         $loader->load('service.yml');
     }
 
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,8 +10,14 @@ class Configuration
     public function getConfigTree()
     {
         $builder = new TreeBuilder();
+
         $root = $builder->root('ab');
-        $root->children()->scalarNode('db_driver')->isRequired()->cannotBeEmpty()->end();
+
+        $root->children()
+            ->scalarNode('db_driver')->isRequired()->cannotBeEmpty()->end()
+            ->scalarNode('model_class')->defaultNull()->end()
+            ->scalarNode('model_repository')->defaultNull()->end()
+        ;
 
         return $builder->buildTree();
     }
